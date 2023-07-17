92dff6db7b0d2036c53832eef1e3f08c604c760b
definoob <noahjun511@gmail.com>
2023-07-17 08:18:16 -0400

    hello world

---
+++ src/test.rs
@@ -0,0 +1,14 @@
+use crate::{Contract, ContractClient};
+use soroban_sdk::{symbol_short, vec, Env, Symbol};
+
+#[test]
+fn test() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Contract);
+    let client = ContractClient::new(&env, &contract_id);
+    let words = client.hello(&symbol_short!("Dev"));
+    assert_eq!(
+        words,
+        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
+    );
+}
